08db9c6d4f1116d7e4f327c478b309ad523c9646
MattSegal <mattdsegal@gmail.com>
2016-11-13 02:39:23 +1100

    refactor knapsack, potential performance improvements

--- knapsack problem/knapsack.py
+++ knapsack problem/knapsack.py
@@ -7,159 +7,71 @@ uses either BFS (queue) or DFS (stack) to navigate solution tree
 uses a value estimating heuristic to prune sub-optimal nodes and reduce the search space
 """
 
-import numpy as np  # NumericalPython for array functions
-import time         # for timing optimisation process
+import sys
+import time
+import numpy as np
 
-from knapsack_data_structures import Queue
-from knapsack_data_structures import Stack
+from queue import Queue
+from stack import Stack
 
+def solve_it(input_text):
 
-# UNIT TESTS
-def test_parse_input():
-    input_data = "4 11\n8 4\n10 5\n15 8\n4 3\n"
-    (value,weight,num_items,capacity) = parse_input(input_data)
-    assert num_items == 4
-    assert capacity == 11
-    # value and weight sorted by denisty
-    assert (value == np.array([10,8,15,4])).all()
-    assert (weight == np.array([5,4,8,3])).all()
-    
-    input_data = "3 15\n1 1\n1 2\n1 3\n"
-    (value,weight,num_items,capacity) = parse_input(input_data)
-    assert num_items == 3
-    assert capacity == 15
-    # value and weight sorted by denisty    
-    assert (value == np.array([1,1,1])).all()
-    assert (weight == np.array([1,2,3])).all()
-    
+    kp = KnapsackProblemData(input_text)
 
-    input_data = "3 20\n1 1\n2 1\n3 1\n"
-    (value,weight,num_items,capacity) = parse_input(input_data)
-    assert num_items == 3
-    assert capacity == 20
-    # value and weight sorted by denisty    
-    assert (value == np.array([3,2,1])).all()
-    assert (weight == np.array([1,1,1])).all()
-    
-
-def test_estimate_value():
-    input_data = "4 11\n8 4\n10 5\n15 8\n4 3\n"
-    (value,weight,num_items,capacity) = parse_input(input_data)
-    choice = [1,1,0]
-    # value vector  [ 10 8  15 4 ]
-    # weight vector [ 5  4  8  3 ]
-    # choice gets weight 5+4 = 9 with remaining capacity 2
-    # choice gets value 10+8 = 18
-    # we can take 2/3 of item 4, giving us added value of (2/3)*4 = 2.67
-    # estimated value == 20.67
-    estimated_value = estimate_value(capacity,value,weight,choice)
-    assert choice == [1,1,0] # choice should not mutate
-    assert estimated_value == 18 + (2*4)/float(3)
-
-    choice = [1,0]
-    # value vector  [ 10 8  15 4 ]
-    # weight vector [ 5  4  8  3 ]
-    # choice gets weight 5 = 5 with remaining capacity 6
-    # choice gets value 10 = 10
-    # we can take 6/8 of item 3, giving us added value of (3/4)*15 = 11.25
-    # estimated value == 21.25
-    estimated_value = estimate_value(capacity,value,weight,choice)
-    assert choice == [1,0] # choice should not mutate
-    assert estimated_value == 21.25
-
-    choice = [0,0]
-    # value vector  [ 10 8  15 4 ]
-    # weight vector [ 5  4  8  3 ]
-    # choice gets weight 0 with remaining capacity 11
-    # choice gets value 0
-    # we take all of item 3
-    #	value = 15
-    #	weight = 8
-    # we can take all of item 4, giving us added value of 4
-    # estimated value == 19
-    estimated_value = estimate_value(capacity,value,weight,choice)
-    assert choice == [0,0] # choice should not mutate
-    assert estimated_value == 19
-
-
-def test_solve_it():
-    # 4 item problem
-    input_data = "4 11\n8 4\n10 5\n15 8\n4 3\n"
-    
-    best_value = 19.0
-    optimal = 1
-    best_choice = [0,0,1,1]
-    output_data = str(best_value) + ' ' + str(optimal) + '\n'
-    output_data += str(best_choice)
-    
-    result = solve_it(input_data)
-    assert result == output_data
-
-    # 19 item problem
-    input_data = "19 31181\n1945 4990\n321 1142\n2945 7390\n4136 10372\n1107 3114\n1022 2744\n1101 3102\n2890 7280\n962 2624\n1060 3020\n805 2310\n689 2078\n1513 3926\n3878 9656\n13504 32708\n1865 4830\n667 2034\n1833 4766\n16553 40006"
-
-    best_value = 12248.0
-    optimal = 1
-    best_choice = [0,0,1,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0,0]
-    output_data = str(best_value) + ' ' + str(optimal) + '\n'
-    output_data += str(best_choice)
-    result = solve_it(input_data)
-    assert result == output_data
-
-# =============================================================================== #
-
-def solve_it(input_data):
-    
-    (value,weight,num_items,capacity) = parse_input(input_data)
+    print 'Number of items   {0}'.format(kp.num_items) 
+    print 'Knapsack capacity {0}'.format(kp.capacity)
 
     start = time.time()
     
-    print 'number of items = %d' % num_items 
-    print 'capacity = %d' % capacity
+    # Pre sort weights and values by density ( value / kg, highest to lowest)
+    density         = np.divide(kp.values,kp.weights.astype(float))
+    density_order   = np.lexsort((np.zeros(len(density)),density))
 
-    best_value    = 0       
-    best_weight  = None
-    best_choice  = None
+    kp.weights      = kp.weights[density_order[::-1]]
+    kp.values       = kp.values[density_order[::-1]]
 
     # ===== Branch and Bound Search ===== #
+    best_value    = 0 
 
-    stack = Stack() # set to Stack() for DFS or Queue for BFS
-    stack.push([0])
-    stack.push([1])
+    stack = Stack() # Set to Stack for DFS or Queue for BFS
+    stack.push([0]) # Initial choice of don't take the first item
+    stack.push([1]) # Initial choice of take the first item
 
-    # loop until all branches explored/pruned - when there are no items left in stack
+    # Loop until all branches have been explored or pruned
     while len(stack) > 0:
-	choice = stack.pop()
+        choice = stack.pop()
 
-	current_weight = np.sum(np.multiply(weight[0:len(choice)],np.array(choice)))
-	over_capacity = current_weight > capacity
-	
-	if over_capacity:
-	    # prune node, search elsewhere
+        num_chosen = len(choice)
+
+        # TODO: Try memoizing these values and pushing them onto the stack
+        current_weight = np.sum(np.multiply(kp.weights[0:num_chosen],np.array(choice)))
+        current_value  = np.sum(np.multiply(kp.values[0:num_chosen],np.array(choice)))
+        
+        if current_weight > kp.capacity:
+            # Prune the node, search elsewhere
             continue
+
+        node_is_leaf = num_chosen == kp.num_items
+        
+        if node_is_leaf:
+            current_value_is_best = current_value > best_value
+
+            if current_value_is_best:
+                best_value  = current_value 
+                best_weight = current_weight  
+                best_choice = choice
         else:
-            node_is_leaf = len(choice) == num_items
-            if node_is_leaf:
-		current_value = np.sum(np.multiply(value,np.array(choice)))
-                new_best_value = current_value > best_value
-                if new_best_value:
-		    best_value = current_value 
-                    best_weight = current_weight  
-                    best_choice = choice
-            else:
-                # estimate upper bound for knapsack value
-                estimated_value = estimate_value(capacity,value,weight,choice);
-                if estimated_value < best_value:
-		    # prune node, search elsewhere
-		    continue
-                else:
-		    # keep exploring down the tree from the node using DFS or BFS
-		    choice.append(0)
-		    stack.push(choice[:])
-		    choice[-1] = 1
-		    stack.push(choice[:])
-			
-    # ===== Search Complete, Return Values ===== #
+            # Estimate upper bound for knapsack's value given current choices
+            estimated_value = estimate_value(kp,choice,current_weight,current_value)
+            estimated_value_is_suboptimal = estimated_value < best_value
+            
+            if estimated_value_is_suboptimal:
+                # Prune the node, search elsewhere
+                continue
+        
+            # Keep exploring the tree from this node
+            stack.push(choice + [0]) # Try not choosing next item
+            stack.push(choice + [1]) # Try choosing next item
 
     end = time.time()
     print "Time taken (s) = ",int((end-start))
@@ -173,80 +85,62 @@ def solve_it(input_data):
 
 # =============================================================================== #
 
-def parse_input(input_data):
+def estimate_value(kp,choice,current_weight,current_value):
     """
-    returns input data in a more easy to process format
+    Calculate upper bound on value to be obtained for a given choice.
+    Select each item greedily by value density - already sorted highest to lowest
     """
-    lines = input_data.split('\n')                 
-    firstLine = lines[0].split()    
-    num_items = int(firstLine[0])           
-    capacity = int(firstLine[1])          
 
-    value = []
-    weight = []
-    
-    for item in range(1, num_items+1):	# for i = [1 to n+1]
-        line = lines[item]              # line = 'vi wi'
-        parts = line.split()            # parts = ['vi','wi']
-        value.append(int(parts[0]))     # value vector [ v0 .... v(n-1) ]
-        weight.append(int(parts[1]))    # weight vector [ w0 .... w(n-1) ]
+    num_chosen    = len(choice)
+    num_to_choose = len(kp.values)
 
-    value = np.array(value)
-    weight = np.array(weight)
+    mass_knapsack  = current_weight
+    assert mass_knapsack <= kp.capacity
+    value_knapsack = current_value
 
-    # calculate density
-    density = np.divide(value.astype(float),weight.astype(float)) # density vector
-    # sort from lowest to highest density - gives an np.array of indices of items sorted by density
-    density_order = np.lexsort((np.zeros(len(density)),density))
+    for item_idx in range(num_chosen,num_to_choose):
 
-    value_sorted = np.zeros(num_items)
-    weight_sorted = np.zeros(num_items)
+        # Pick items using value greed, with a fracton of the last item
+        item_weight = kp.weights[item_idx]
+        item_value  = kp.values[item_idx]
+        new_mass = mass_knapsack + item_weight
 
-    # copy value and weight list into numpy array in density sorted order (highest to lowest)
-    sorted_index = 0
-    for index in reversed(density_order):
-	value_sorted[sorted_index] = value[index]
-	weight_sorted[sorted_index] = weight[index]
-	sorted_index += 1
+        if new_mass <= kp.capacity:
+            # Keep adding items
+            value_knapsack += item_value
+            mass_knapsack  = new_mass
 
-    return (value_sorted,weight_sorted,num_items,capacity)    
+        else:
+            # Add a fraction of the last item to get upper bound
+            remaining_mass   = kp.capacity - mass_knapsack
+            item_fraction    = remaining_mass / float(item_weight)
+            value_knapsack  += item_value  * item_fraction
+            mass_knapsack   += item_weight * item_fraction
+            break
+
+    return value_knapsack
 
 # =============================================================================== #
 
-def estimate_value(capacity,value,weight,choice):
-    """
-    select each item by by value density - already sorted
-    select fraction of last available item using linear relaxation
-    CONSIDER:
-	you can trade time for memory by storing past results for this function
-	and re using them in future calls - could use a stack in parallel to choice stack
-	not 100% on how to implement, but this may help with a likely bottleneck
-    """
-    choice = choice[:] # so choice doesn't get mutated
-    choices_remaining = len(value) - len(choice)
-    for undecided_choice in range(choices_remaining):
-	choice.append(1)    # take the item for all undecided choices
+class KnapsackProblemData:
+    def __init__(self,input_text):
+        # eg, input_text = "4 11\n8 4\n10 5\n15 8\n4 3\n"
 
-    # pick items using value greed, with a fracton of the last item
-    mass_knap = 0	# mass of knapsack
-    value_knap = 0	# sum of value in knapsack
-    for item in range(len(choice)):
-        if choice[item] == 1:
-            if (mass_knap + weight[item]) <= capacity:
-                value_knap += value[item]
-                mass_knap += weight[item]
-            elif mass_knap < capacity:
-		item_fraction = (capacity-mass_knap)/float(weight[item])
-                value_knap += value[item]*item_fraction
-                mass_knap += weight[item]*item_fraction
-            elif mass_knap >= capacity:
-                break
+        lines = input_text.strip('\n').split('\n')                 
+        (self.num_items,self.capacity) = (int(num) for num in lines[0].split())
 
-    return value_knap
+        values = []
+        weights = []
+        
+        for line in lines[1:]:
+            (value,weight) = (int(num) for num in line.split())
+            values.append(value)
+            weights.append(weight)
+
+        self.values = np.array(values)
+        self.weights = np.array(weights)
 
 # =============================================================================== #
-
-import sys
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:                                   # opens data file if data file is given as the first argument eg. python solver.py ./data/ks_4_0
---
+++ knapsack problem/unit_tests.py
@@ -0,0 +1,96 @@
+"""
+Run using 
+    py.test unit_tests.py
+"""
+
+import numpy as np
+from data_repository import KnapsackProblemData
+from knapsack import estimate_value, solve_it
+
+
+def test_parse_input_1():
+    input_text = "4 11\n8 4\n10 5\n15 8\n4 3\n"
+    kp = KnapsackProblemData(input_text)
+    assert kp.num_items == 4
+    assert kp.capacity == 11
+    assert (kp.values == np.array([8,10,15,4])).all()
+    assert (kp.weights == np.array([4,5,8,3])).all()
+
+def test_parse_input_2():
+    input_text = "3 15\n1 1\n1 2\n1 3\n"
+    kp = KnapsackProblemData(input_text)
+    assert kp.num_items == 3
+    assert kp.capacity == 15    
+    assert (kp.values == np.array([1,1,1])).all()
+    assert (kp.weights == np.array([1,2,3])).all()
+    
+
+def test_parse_input_3():
+    input_text = "3 20\n1 1\n2 1\n3 1\n"
+    kp = KnapsackProblemData(input_text)
+    assert kp.num_items == 3
+    assert kp.capacity == 20    
+    assert (kp.values == np.array([1,2,3])).all()
+    assert (kp.weights == np.array([1,1,1])).all()
+    
+def test_estimate_value_1():
+    input_text = "4 11\n8 4\n10 5\n15 8\n4 3\n"
+    choice = [1,1,0]
+    kp = KnapsackProblemData(input_text)
+    num_chosen = len(choice)
+    current_weight = np.sum(np.multiply(kp.weights[0:num_chosen],np.array(choice)))
+    current_value  = np.sum(np.multiply(kp.values[0:num_chosen],np.array(choice)))
+    # value vector  [ 10 8  15 4 ]
+    # weight vector [ 5  4  8  3 ]
+    # choice gets weight 5+4 = 9 with remaining capacity 2
+    # choice gets value 10+8 = 18
+    # we can take 2/3 of item 4, giving us added value of (2/3)*4 = 2.67
+    # estimated value == 20.67
+    estimated_value = estimate_value(kp,choice,current_weight,current_value)
+    assert choice == [1,1,0] # choice should not mutate
+    assert estimated_value == 18 + (2*4)/float(3)
+
+def test_estimate_value_2():
+    input_text = "4 11\n8 4\n10 5\n15 8\n4 3\n"
+    choice = [0,0]
+    kp = KnapsackProblemData(input_text)
+    num_chosen = len(choice)
+    current_weight = np.sum(np.multiply(kp.weights[0:num_chosen],np.array(choice)))
+    current_value  = np.sum(np.multiply(kp.values[0:num_chosen],np.array(choice)))
+    # value vector  [ 10 8  15 4 ]
+    # weight vector [ 5  4  8  3 ]
+    # choice gets weight 0 with remaining capacity 11
+    # choice gets value 0
+    # we take all of item 3
+    #	value = 15
+    #	weight = 8
+    # we can take all of item 4, giving us added value of 4
+    # estimated value == 19
+    estimated_value = estimate_value(kp,choice,current_weight,current_value)
+    assert choice == [0,0] # choice should not mutate
+    assert estimated_value == 19
+
+def test_solve_it_4_items():
+    # 4 item problem
+    input_text = "4 11\n8 4\n10 5\n15 8\n4 3\n"
+    
+    best_value = 19
+    optimal = 1
+    best_choice = [0,0,1,1]
+    output_data = str(best_value) + ' ' + str(optimal) + '\n'
+    output_data += str(best_choice)
+    
+    result = solve_it(input_text)
+    assert result == output_data
+
+def test_solve_it_19_items():
+    # 19 item problem
+    input_text = "19 31181\n1945 4990\n321 1142\n2945 7390\n4136 10372\n1107 3114\n1022 2744\n1101 3102\n2890 7280\n962 2624\n1060 3020\n805 2310\n689 2078\n1513 3926\n3878 9656\n13504 32708\n1865 4830\n667 2034\n1833 4766\n16553 40006"
+
+    best_value = 12248
+    optimal = 1
+    best_choice = [0,0,1,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0,0]
+    output_data = str(best_value) + ' ' + str(optimal) + '\n'
+    output_data += str(best_choice)
+    result = solve_it(input_text)
+    assert result == output_data
